9fd939e1a3a6f54ee20ec46a5f0249cc9a0dd14d
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-05-13 21:55:46 +0900

    enhance: 通知音とバイブレーションを追加

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# ekimemo_map
+# ekimemo_map_app
 
-A new Flutter project.
+---
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### License
+- [station_database](https://github.com/Seo-4d696b75/station_database) by Seo-4d696b75, [Licensed under CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)  
+- Sound Effect by [UNIVERSFIELD](https://pixabay.com/users/universfield-28281460/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music), [LIECIO](https://pixabay.com/users/liecio-3298866/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music) from [Pixabay](https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music)
--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:vibration/vibration.dart';
 import 'station.dart';
 import 'config.dart';
 import 'utils.dart';
@@ -9,6 +11,7 @@ class NotificationManager {
   NotificationManager._internal();
 
   final _notification = FlutterLocalNotificationsPlugin();
+  final _audioPlayer = AudioPlayer();
 
   Future<void> initialize() async {
     await _notification.initialize(const InitializationSettings(android: AndroidInitializationSettings('ic_launcher')));
@@ -16,9 +19,17 @@ class NotificationManager {
 
   Future<void> showNotification(String title, String body) async {
     if (!Config.enableNotification) return;
-    const android = AndroidNotificationDetails('nearest_station', '最寄り駅通知', importance: Importance.high, priority: Priority.high);
+    const android = AndroidNotificationDetails('nearest_station', '最寄り駅通知',
+      importance: Importance.high,
+      priority: Priority.high,
+      playSound: false,
+      enableVibration: false,
+    );
     const platform = NotificationDetails(android: android);
     await _notification.show(0, title, body, platform);
+    
+    _audioPlayer.play(AssetSource('sound/notification_1.mp3')); // TODO: 通知音を変更できるように
+    Vibration.vibrate(pattern: [0, 100, 200, 100, 100, 300, 150, 100, 200, 100, 100, 300]); // TODO: パターンを変更できるように
   }
 
   Future<void> showStationNotification(StationData data, {bool reNotify = false}) async {
