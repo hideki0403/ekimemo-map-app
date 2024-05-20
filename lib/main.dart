06f7b041dc833d95dcaf6008621dd0dfb739cb40
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-05-21 01:05:06 +0900

    fix: dynamic_colorで返される色が意図していないものになっていた問題を修正

--- lib/main.dart
+++ lib/main.dart
@@ -55,58 +55,45 @@ class Root extends StatelessWidget {
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
       // TODO: テーマを変えられるように
-      builder: (lightColorScheme, darkColorScheme) {
-        ColorScheme? lightScheme, darkScheme;
-
-        if (lightColorScheme != null && darkColorScheme != null) {
-          final (light, dark) = _generateDynamicColourSchemes(lightColorScheme, darkColorScheme);
-          lightScheme = light;
-          darkScheme = dark;
-        } else {
-          lightScheme = lightColorScheme;
-          darkScheme = darkColorScheme;
-        }
-
-        return MaterialApp.router(
-          theme: _buildTheme(Brightness.light, lightScheme),
-          darkTheme: _buildTheme(Brightness.dark, darkScheme),
-          routerConfig: GoRouter(
-            navigatorKey: navigatorKey,
-            initialLocation: '/',
-            routes: [
-              GoRoute(
-                path: '/',
-                builder: (context, state) => const HomeView(),
-              ),
-              GoRoute(
-                path: '/settings',
-                builder: (context, state) => const SettingsView(),
-              ),
-              GoRoute(
-                path: '/assistant-flow',
-                builder: (context, state) => const AssistantFlowView(),
-              ),
-              GoRoute(
-                path: '/assistant-choose-rect',
-                builder: (context, state) => const AssistantChooseRectView(),
-              ),
-              GoRoute(
-                path: '/map',
-                builder: (context, state) {
-                  final params = state.uri.queryParameters;
-                  return MapView(stationId: params['station-id'], lineId: params['line-id']);
-                },
-              ),
-              GoRoute(
-                path: '/station',
-                builder: (context, state) {
-                  return StationDetailView(stationId: state.uri.queryParameters['id']);
-                },
-              ),
-            ],
-          ),
-        );
-      }
+      builder: (lightColorScheme, darkColorScheme) => MaterialApp.router(
+        theme: _buildTheme(Brightness.light, lightColorScheme),
+        darkTheme: _buildTheme(Brightness.dark, darkColorScheme),
+        routerConfig: GoRouter(
+          navigatorKey: navigatorKey,
+          initialLocation: '/',
+          routes: [
+            GoRoute(
+              path: '/',
+              builder: (context, state) => const HomeView(),
+            ),
+            GoRoute(
+              path: '/settings',
+              builder: (context, state) => const SettingsView(),
+            ),
+            GoRoute(
+              path: '/assistant-flow',
+              builder: (context, state) => const AssistantFlowView(),
+            ),
+            GoRoute(
+              path: '/assistant-choose-rect',
+              builder: (context, state) => const AssistantChooseRectView(),
+            ),
+            GoRoute(
+              path: '/map',
+              builder: (context, state) {
+                final params = state.uri.queryParameters;
+                return MapView(stationId: params['station-id'], lineId: params['line-id']);
+              },
+            ),
+            GoRoute(
+              path: '/station',
+              builder: (context, state) {
+                return StationDetailView(stationId: state.uri.queryParameters['id']);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -126,39 +113,3 @@ ThemeData _buildTheme(Brightness brightness, ColorScheme? colorScheme) {
     ),
   );
 }
-
-// https://github.com/material-foundation/flutter-packages/issues/582
-(ColorScheme light, ColorScheme dark) _generateDynamicColourSchemes(ColorScheme lightDynamic, ColorScheme darkDynamic) {
-  var lightBase = ColorScheme.fromSeed(seedColor: lightDynamic.primary);
-  var darkBase = ColorScheme.fromSeed(seedColor: darkDynamic.primary, brightness: Brightness.dark);
-
-  var lightAdditionalColours = _extractAdditionalColours(lightBase);
-  var darkAdditionalColours = _extractAdditionalColours(darkBase);
-
-  var lightScheme = _insertAdditionalColours(lightBase, lightAdditionalColours);
-  var darkScheme = _insertAdditionalColours(darkBase, darkAdditionalColours);
-
-  return (lightScheme.harmonized(), darkScheme.harmonized());
-}
-
-List<Color> _extractAdditionalColours(ColorScheme scheme) => [
-  scheme.surface,
-  scheme.surfaceDim,
-  scheme.surfaceBright,
-  scheme.surfaceContainerLowest,
-  scheme.surfaceContainerLow,
-  scheme.surfaceContainer,
-  scheme.surfaceContainerHigh,
-  scheme.surfaceContainerHighest,
-];
-
-ColorScheme _insertAdditionalColours(ColorScheme scheme, List<Color> additionalColours) => scheme.copyWith(
-  surface: additionalColours[0],
-  surfaceDim: additionalColours[1],
-  surfaceBright: additionalColours[2],
-  surfaceContainerLowest: additionalColours[3],
-  surfaceContainerLow: additionalColours[4],
-  surfaceContainer: additionalColours[5],
-  surfaceContainerHigh: additionalColours[6],
-  surfaceContainerHighest: additionalColours[7],
-);
