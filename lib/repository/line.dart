83055ffe000e472d68aab0e59382b939292c0675
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-05-28 21:08:10 +0900

    remove: 不必要なimportを削除

--- lib/repository/line.dart
+++ lib/repository/line.dart
@@ -1,7 +1,5 @@
 import '_abstract.dart';
 import 'package:ekimemo_map/models/line.dart';
-import 'package:ekimemo_map/repository/station.dart';
-
 
 class LineRepository extends AbstractRepository<Line> {
   LineRepository() : super(Line(), 'line', 'code');
