51a017fb66ceb3f878a0f4c823fc3a152f58cdf0
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-05-31 02:23:18 +0900

    remove: url_launcherを削除

--- lib/ui/pages/settings.dart
+++ lib/ui/pages/settings.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:ekimemo_map/services/updater.dart';
