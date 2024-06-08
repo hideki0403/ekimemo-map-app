ed765a845cfc5b43644482afc501aac6d757ec14
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-06-08 20:16:50 +0900

    add: controller用Sliderを実装

---
+++ lib/ui/widgets/cb_slider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CbSlider extends StatefulWidget {
+  const CbSlider({
+    super.key,
+    required this.defaultValue,
+    required this.min,
+    required this.max,
+    required this.onChanged,
+    this.disabled = false,
+  });
+
+  final int defaultValue;
+  final int min;
+  final int max;
+  final ValueChanged<int> onChanged;
+  final bool disabled;
+
+  @override
+  State<CbSlider> createState() => _CbSliderState();
+}
+
+class _CbSliderState extends State<CbSlider> {
+  double _value = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _value = widget.defaultValue.toDouble();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _value,
+      min: widget.min.toDouble(),
+      max: widget.max.toDouble(),
+      divisions: widget.max - widget.min,
+      label: _value.toInt().toString(),
+      onChanged: widget.disabled ? null : (value) {
+        setState(() {
+          _value = value;
+        });
+        widget.onChanged(value.toInt());
+      },
+    );
+  }
+}
