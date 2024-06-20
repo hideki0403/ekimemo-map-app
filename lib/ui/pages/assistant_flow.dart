1d94e0e7eb7e67c5dd094a547a6a47e90436aef7
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-06-20 19:36:46 +0900

    feat: AssistantにImport/Exportを実装

--- lib/ui/pages/assistant_flow.dart
+++ lib/ui/pages/assistant_flow.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+
 import 'package:ekimemo_map/services/assistant.dart';
 import 'package:ekimemo_map/services/utils.dart';
 import 'package:ekimemo_map/ui/widgets/editor_dialog.dart';
@@ -97,8 +100,61 @@ class _AssistantFlowViewState extends State<AssistantFlowView> {
       ),
       body: CustomScrollView(
         slivers: [
-          const SliverAppBar.large(
-            title: Text('Assistant Flow'),
+          SliverAppBar.large(
+            title: const Text('Assistant Flow'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.file_upload),
+                onPressed: () async {
+                  final result = await showYesNoDialog(
+                    title: 'Export',
+                    message: 'Copy the assistant flow to the clipboard?',
+                  );
+                  if (result != true) return;
+
+                  final data = AssistantFlowUtils.stringify(_data);
+                  await Clipboard.setData(ClipboardData(text: base64Encode(utf8.encode(data))));
+
+                  if (context.mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Copied to clipboard'))
+                    );
+                  }
+                }
+              ),
+              IconButton(
+                icon: const Icon(Icons.file_download),
+                onPressed: () async {
+                  final result = await showEditorDialog(
+                    title: 'Import',
+                    caption: 'Paste the exported assistant flow code here',
+                    type: EditorDialogType.text,
+                  );
+                  if (result == null) return;
+
+                  try {
+                    final data = utf8.decode(base64Decode(result));
+                    final items = AssistantFlowUtils.parse(data);
+                    setState(() {
+                      _data = items;
+                      AssistantFlow.set(_data);
+                    });
+
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Imported'))
+                      );
+                    }
+                  } catch (e) {
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Invalid data'))
+                      );
+                    }
+                  }
+                }
+              ),
+            ],
           ),
           SliverPadding(
             padding: const EdgeInsets.only(left: 20, right: 20),
