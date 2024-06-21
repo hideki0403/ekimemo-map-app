c57938c01ddc29f3c0507c5e620f4326571da4ca
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-06-21 20:09:56 +0900

    enhance: StationSimpleに属性情報を表示するオプションを追加

--- lib/ui/widgets/station_simple.dart
+++ lib/ui/widgets/station_simple.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+import 'package:ekimemo_map/services/utils.dart';
 import 'package:ekimemo_map/models/station.dart';
 
 class StationSimple extends StatelessWidget {
   final Station station;
   final bool isAccessed;
-  const StationSimple({required this.station, required this.isAccessed, super.key});
+  final bool showAttr;
+  const StationSimple({required this.station, this.isAccessed = false, this.showAttr = false, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +24,10 @@ class StationSimple extends StatelessWidget {
           padding: const EdgeInsets.only(left: 20, top: 12, bottom: 12, right: 20),
           child: Row(
             children: [
+              if (showAttr) ...[
+                getAttrIcon(station.attr, context: context),
+                const SizedBox(width: 20),
+              ],
               Expanded(
                 child: Text(station.name, textScaler: const TextScaler.linear(1.2)),
               ),
