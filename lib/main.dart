a1693985a502ed9b1ab3bd3c43f8c246332af96d
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-05-17 02:48:07 +0900

    refactor: dynamic_colorからdynamic_system_colorsに移行

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:dynamic_color/dynamic_color.dart';
+import 'package:dynamic_system_colors/dynamic_system_colors.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
